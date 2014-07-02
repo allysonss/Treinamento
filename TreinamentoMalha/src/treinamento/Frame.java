442700c465e5ae044ab44e4d0eb38c3b557d3f0e
allysonss <srs.a94@gmail.com>
2014-07-02 15:46:49 -0300

    Commitando com interface parcialemente reparada

--- TreinamentoMalha/src/treinamento/Frame.java
+++ TreinamentoMalha/src/treinamento/Frame.java
@@ -2,7 +2,6 @@ package treinamento;
 
 import java.awt.Dimension;
 import java.awt.GridBagLayout;
-import java.awt.GridLayout;
 import javax.swing.BorderFactory;
 import javax.swing.JButton;
 import javax.swing.JComboBox;
@@ -32,30 +31,37 @@ public class Frame extends JFrame {
 		JLabel validLabel = new JLabel("Validade:");
 		
 		JComboBox<String> localCB = new JComboBox<String>();
-		localCB.setSize(100, 25);
+		localCB.setMinimumSize(new Dimension(260, 20));
+		localCB.setPreferredSize(localCB.getMinimumSize());
+		
 		JComboBox<String> grupCB = new JComboBox<String>();
+		grupCB.setMinimumSize(new Dimension(260, 20));
+		grupCB.setPreferredSize(grupCB.getMinimumSize());
+		
 		JTextField loteTF = new JTextField();
+		loteTF.setMinimumSize(new Dimension(200, 20));
+		loteTF.setPreferredSize(loteTF.getMinimumSize());
 		JTextField validTF = new JTextField();
 		JButton novoButton = new JButton("Novo");
 		
 		topPanel = new JPanel(new GridBagLayout());
 		topPanel.setBorder(BorderFactory.createTitledBorder("Criar Nova Rota"));
-		topPanel.add(localLabel, new GBC(0, 0).insets(0, -300, 0, 0).west());
-		topPanel.add(localCB, new GBC(1, 0).insets(0, 20, 0, 0).west());
-		topPanel.add(grupLabel, new GBC(0, 1).insets(20, -300, 0, 0).west());
+		topPanel.add(localLabel, new GBC(0, 0).insets(0, -200, 0, 0).none().west());
+		topPanel.add(localCB, new GBC(1, 0).insets(0, 20, 0, 0).none().west());
+		topPanel.add(grupLabel, new GBC(0, 1).insets(20, -200, 0, 0).west());
 		topPanel.add(grupCB, new GBC(1, 1).insets(20, 20, 0, 0).west());
-		topPanel.add(loteLabel, new GBC(0, 2).insets(20, -300, 0, 0).west());
+		topPanel.add(loteLabel, new GBC(0, 2).insets(20, -200, 0, 0).west());
 		topPanel.add(loteTF, new GBC(1,2).insets(0, 20, 0, 0).west());
-		topPanel.add(validLabel, new GBC(0,3).insets(20, -300, 0, 0).west());
+		topPanel.add(validLabel, new GBC(0,3).insets(20, -200, 0, 0).west());
 		topPanel.add(validTF, new GBC(1, 3).insets(0, 20, 0, 0).west());
-		topPanel.add(novoButton, new GBC(3,3).insets(0, 20, 0, -300).east());
+		topPanel.add(novoButton, new GBC(3,3).insets(0, 20, 0, -200).east());
 		
 		
-		downPanel = new JPanel(new GridLayout(2, 1));
+		downPanel = new JPanel(new GridBagLayout());
 		
-		mainPanel = new JPanel(new GridLayout(2, 1));
-		mainPanel.add(topPanel);
-		mainPanel.add(downPanel);
+		mainPanel = new JPanel(new GridBagLayout());
+		mainPanel.add(topPanel, new GBC(0,0).both());
+		mainPanel.add(downPanel, new GBC(0,1).both());
 		
 		mainFrame = new JFrame();
 		mainFrame.add(mainPanel);
